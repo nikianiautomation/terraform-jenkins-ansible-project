59567381e074aad72a8c41585bd79976dcaff231
nikiani <nikianiautomation@gmail.com>
2025-12-02 16:31:37 +0530

    updated jenkins groovy

--- jenkins/resources/docker_jenkins_maven_installation.sh
+++ jenkins/resources/docker_jenkins_maven_installation.sh
@@ -15,6 +15,7 @@ sudo systemctl start jenkins
 sudo yum update -y
 sudo yum install git -y
 sudo yum install -y docker
+sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 sudo systemctl enable docker
 
